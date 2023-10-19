8ce48c381e235b37ccfe9a455c26700afd39273e
devsula50 <devsula50@gmail.com>
2023-10-19 17:36:15 +0900

    Resetting the Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -15,11 +15,8 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
+let scores, currentScore, activePlayer, playing;
 init();
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 const switchPlayer = () => {
   currentScore = 0;
@@ -63,7 +60,7 @@ btnHold.addEventListener('click', () => {
 
     // 2. Check if player's score is >= 100
     // Finish the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
@@ -80,8 +77,23 @@ btnHold.addEventListener('click', () => {
   }
 });
 
+btnNew.addEventListener('click', init);
+
 function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
   score0El.textContent = 0;
   score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
   diceEl.classList.add('hidden');
 }
