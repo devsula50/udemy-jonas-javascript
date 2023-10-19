780c60c9de00827b6893d2aca894aef25d41f1d2
devsula50 <devsula50@gmail.com>
2023-10-19 12:58:20 +0900

    Slecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelector('.message');
+document.querySelector('.message').textContent = '🎉Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
