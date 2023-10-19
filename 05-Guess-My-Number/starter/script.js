00408f137fa5e87fed6af6d537bb7ba6f30c79ec
devsula50 <devsula50@gmail.com>
2023-10-19 13:08:05 +0900

    Handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,3 +5,15 @@ document.querySelector('.message').textContent = '🎉Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // const answer = Number(document.querySelector('.number').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+
+  // if (guess === answer) console.log('correct');
+  // else console.log('not correct');
+});
