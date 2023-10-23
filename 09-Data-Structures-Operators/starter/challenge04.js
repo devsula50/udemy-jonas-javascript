226713368ca9735edee591d4422ef28dd92b140d
devsula50 <devsula50@gmail.com>
2023-10-23 10:05:10 +0900

    Data Structures, Modern Operators and Strings Challenges0~4

--- 09-Data-Structures-Operators/starter/challenge04.js
+++ 09-Data-Structures-Operators/starter/challenge04.js
@@ -1,20 +1,24 @@
 //Coding Challenge #4
 // Write a program that receives a list of variable names written in underscore_case
 // and convert them to camelCase.
+
 // The input will come from a textarea inserted into the DOM (see code below to
 // insert the elements), and conversion will happen when the button is pressed.
+
 // Test data (pasted to textarea, including spaces):
 // underscore_case
 // first_name
 // Some_Variable
 //  calculate_AGE
 // delayed_departure
+
 // Should produce this output (5 separate console.log outputs):
 // underscoreCase ✅
 // firstName ✅✅
 // someVariable ✅✅✅
 // calculateAge ✅✅✅✅
 // delayedDeparture ✅✅✅✅✅
+
 // Hints:
 // § Remember which character defines a new line in the textarea �
 // § The solution only needs to work for a variable made out of 2 words, like a_b
@@ -22,4 +26,20 @@
 // name conversion working �
 // § This challenge is difficult on purpose, so start watching the solution in case
 // you're stuck. Then pause and continue!
-// Afterwards, test with your own test data!
+// Afterwards, test with your own test data! 😉
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', () => {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    console.log(row, first, second);
+
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
